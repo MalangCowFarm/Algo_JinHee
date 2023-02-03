843edca625e1c3dd2175e35b198b4c485c796365
sokurii <wlsl0620@naver.com>
2023-02-03 21:43:45 +0900

    week1

--- 230203/S1620.py
+++ 230203/S1620.py
@@ -1,35 +1,19 @@
-# 런타임 에러로 포기합니다.. 
 import sys
-input = sys.stdin.readline 
 
 n, m = map(int,input().split())
 
 pokemon = {}
 
 for i in range(1, n+1):
-    p = input()
+    p = sys.stdin.readline().strip()
     pokemon[i] = p
     pokemon[p] = i 
 
 for _ in range(m):
-    answer = input()
+    answer = sys.stdin.readline().strip()
     if answer.isdigit():
-        print(pokemon[int(answer-1)])
+        print(pokemon[int(answer)])
     else:
-        print(pokemon[answer]+1)
-
-
-
-# pokemon = []   시간초과 
-# for _ in range(n): # 0 1 2 3 ... 25
-#     p = input()
-#     pokemon.append(p)
-
-# for _ in range(m):
-#     answer = input()
-#     if answer.isdigit():
-#         print(pokemon[int(answer)-1])
-#     else:
-#         print(pokemon.index(answer)+1)
+        print(pokemon[answer])
 
     
