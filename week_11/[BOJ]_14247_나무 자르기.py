cdb767139f50f71412301c428812f669b72e7d1f
sokurii <122415844+sokurii@users.noreply.github.com>
2023-04-11 13:49:50 +0900

    230411 정렬/그리디

--- week_11/[BOJ]_14247_나무 자르기.py
+++ week_11/[BOJ]_14247_나무 자르기.py
@@ -19,7 +19,18 @@ while True:
 
 print(mx)
 
+'''
+성장이 가장 큰 나무는 마지막에 베는 것이 이득
+더딘 순으로 정렬을 먼저 하고 순서대로 베었다
+시간초과에러가 뜰 줄 알았지만 생각보다 짧게 걸렸음! '''
 
-
-
+'''
+다른 사람 코드 : 나무는 왜 정렬 안해줘도 같은 값 나오는 지?'''
+# grow.sort()
+#
+# mx = 0
+# for i in range(n):
+#     mx += tree[i]+grow[i]*i
+#     print(tree[i]+grow[i]*i)
+# print(mx)
 
